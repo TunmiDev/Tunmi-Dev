304d55989e3678ecd1eedb441a3e10b798baa687
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-06-27 05:01:41 +0100

    Fix: corrected image file path casing for deployment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import profile1 from "../assets/profile1.jpg";
-import profile2 from "../assets/profile2.jpg";
+import profile1 from "../assets/Profile1.jpg";
+import profile2 from "../assets/Profile2.jpg";
 
 const Hero = () => {
   return (
