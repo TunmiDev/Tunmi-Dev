369f9441d1a6ac2261c716422781198966a22458
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-07-29 10:20:11 +0100

    updated cloud on mobile screen

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,19 @@
-// src/components/Skills.jsx
 import React from "react";
 import SkillsCloud from "./SkillsCloud";
 import SkillsGroup from "./SkillsGroup";
 
 const Skills = () => {
   return (
-    <section id="skills" className="py-16 pt-10 pb-16  dark:text-white">
-      <div className="container mx-auto px-6 md:flex md:space-x-10 items-start">
+    <section id="skills" className="py-16 pt-10 pb-16 dark:text-white">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row md:space-x-10 items-start h-screen">
         {/* Left: 3D Skills Cloud */}
-        <div className="w-full md:w-1/2 justify-center items-center flex mb-10 md:mb-0">
+        <div className="w-full md:w-1/2 flex justify-center items-center h-1/2 md:h-auto mb-10 md:mb-0">
           <SkillsCloud />
         </div>
 
         {/* Right: Grouped Skills */}
-        <div className="w-full md:w-1/2">
-          <h2 className="text-3xl font-medium mb-6 text-center mt-32 md:text-left pl-8">
+        <div className="w-full md:w-1/2 h-1/2 md:h-auto">
+          <h2 className="text-3xl font-medium mb-6 text-center mt-10 md:mt-32 md:text-left pl-8">
             Skills / Stack
           </h2>
           <SkillsGroup />
