b53f9c25f2e2d1c16247d4624431ffb846c66dbe
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-07-31 08:58:34 +0100

    fixed spacing issues

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,8 +4,8 @@ import SkillsGroup from "./SkillsGroup";
 
 const Skills = () => {
   return (
-    <section id="skills" className="py-16 pt-10 pb-16 dark:text-white">
-      <div className="container mx-auto px-6 flex flex-col md:flex-row md:space-x-10 items-start h-screen">
+    <section id="skills" className="py-16 pt-10 pb-0 dark:text-white">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row md:space-x-10 items-start">
         {/* Left: 3D Skills Cloud */}
         <div className="w-full md:w-1/2 flex justify-center items-center h-1/2 md:h-auto mb-10 md:mb-0">
           <SkillsCloud />
