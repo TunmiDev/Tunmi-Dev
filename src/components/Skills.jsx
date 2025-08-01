0c11492238d6e91a760c2c07175981d7fec71bbb
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-08-01 04:28:54 +0100

    ..

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import SkillsGroup from "./SkillsGroup";
 
 const Skills = () => {
   return (
-    <section id="skills" className="pt-0 pb-0 dark:text-white">
+    <section id="skills" className="pt-8 pb-0 dark:text-white">
       <div className="container mx-auto px-6 flex flex-col md:flex-row md:space-x-10 items-start">
         {/* Left: 3D Skills Cloud */}
         <div className="w-full md:w-1/2 flex justify-center items-center h-1/2 md:h-auto mb-10 md:mb-0">
